6ea0847ddc797dc07b216cb7cf2a5ea159ce9ba3
Pavel Zhukov <cray0000@gmail.com>
2014-12-29 18:37:42 +0100

    initial

---
+++ test/colors.mocha.js
@@ -0,0 +1,16 @@
+var should = require('should');
+var stylus = require('stylus');
+var fs = require('fs');
+var path = require('path');
+
+describe('IBM DL Colors', function(){
+  it('palette()', function(){
+    var inPath = path.normalize(__dirname + '/fixtures/index.styl');
+    var expectedPath = path.normalize(__dirname + '/expected/index.css');
+    var $in = fs.readFileSync(inPath, {encoding: 'utf8'});
+    var $expected = fs.readFileSync(expectedPath, {encoding: 'utf8'});
+    stylus.render($in, {filename: inPath}, function(err, $out){
+      $expected.trim().should.eql( $out.trim() );
+    });
+  });
+});

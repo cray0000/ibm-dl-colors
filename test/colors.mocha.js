9cab3e8650519c6925c73ec9ca48e1e03722b079
Pavel Zhukov <cray0000@gmail.com>
2015-03-14 19:30:37 +0100

    Add support for short syntax (1 -> 10, 5 -> 50, 9 -> 90, 0 -> 100)

--- test/colors.mocha.js
+++ test/colors.mocha.js
@@ -10,6 +10,7 @@ describe('IBM DL Colors', function(){
     var $in = fs.readFileSync(inPath, {encoding: 'utf8'});
     var $expected = fs.readFileSync(expectedPath, {encoding: 'utf8'});
     stylus.render($in, {filename: inPath}, function(err, $out){
+      if (err) throw err;
       $expected.trim().should.eql( $out.trim() );
     });
   });
